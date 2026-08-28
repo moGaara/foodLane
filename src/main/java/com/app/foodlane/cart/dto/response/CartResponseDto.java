8264ee1c142be4e5f6c3015f907bdf1274f22b18
Hajar Ramadan <hajar.ramadan777@gmail.com>
2026-08-28 17:38:26 +0300

    AddToCart : added Response Dtos

--- src/main/java/com/app/foodlane/cart/dto/response/CartResponseDto.java
+++ src/main/java/com/app/foodlane/cart/dto/response/CartResponseDto.java
@@ -1,4 +1,12 @@
 package com.app.foodlane.cart.dto.response;
 
-public record CartResponseDto() {
+import java.math.BigDecimal;
+import java.util.List;
+
+public record CartResponseDto(Long cartId,
+                              Long restaurantId,
+                              String restaurantName,
+                              List<CartItemResponseDto> items,
+                              BigDecimal subtotal) {
+
 }
