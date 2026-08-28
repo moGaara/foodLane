b94c5851f7d146f58328a295f73154863869dc33
Hajar Ramadan <hajar.ramadan777@gmail.com>
2026-08-28 17:36:41 +0300

    AddToCart : add endpoint in the controller

--- src/main/java/com/app/foodlane/cart/controller/CartController.java
+++ src/main/java/com/app/foodlane/cart/controller/CartController.java
@@ -1,16 +1,15 @@
 package com.app.foodlane.cart.controller;
 
-import com.app.foodlane.Auth.entity.Customer;
-import com.app.foodlane.cart.dto.request.AddToCartRequesttDto;
+import com.app.foodlane.cart.dto.request.AddToCartRequestDto;
 import com.app.foodlane.cart.dto.response.CartResponseDto;
 import com.app.foodlane.cart.dto.response.HelloDtoRes;
 import com.app.foodlane.cart.service.IHelloService;
-import com.app.foodlane.cart.service.impl.CartService;
+import com.app.foodlane.cart.service.CartService;
 import com.app.foodlane.utils.CommonFunctions;
 import com.app.foodlane.utils.reswrapper.GenericRes;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -36,21 +35,14 @@ public class CartController {
     }
 
     @PostMapping("/items")
-    public ResponseEntity<GenericRes<CartResponseDto>> addItemToCart
-            (@AuthenticationPrincipal Customer customer , @RequestBody AddToCartRequesttDto requestDto){
-        long id = customer.getCustomerId();
-        CartResponseDto responseDto = cartService.addItem(id , requestDto);
+    public ResponseEntity<GenericRes<CartResponseDto>> addItemToCart(
+            @RequestHeader("Authorization") String authorization,
+            @Valid @RequestBody AddToCartRequestDto requestDto
+    ) {
+        long id = CommonFunctions.extractID(authorization);
+        CartResponseDto responseDto = cartService.addItem(id, requestDto);
         GenericRes<CartResponseDto> res = new GenericRes<>();
         res.setBody(responseDto);
         return ResponseEntity.ok(res);
     }
-
-
-
-
-
-
-
-
-
 }
