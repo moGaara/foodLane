93a31b98be32c381859276b457dd5339fd3a9a33
Hajar Ramadan <hajar.ramadan777@gmail.com>
2026-08-28 18:03:42 +0300

    AddToCart : added CartRepository

--- src/main/java/com/app/foodlane/cart/repository/CartRepository.java
+++ src/main/java/com/app/foodlane/cart/repository/CartRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface CartRepository extends JpaRepository<Cart,Long> {
 
     Optional<Cart> findByStatusAndCustomerCustomerId(String status, Long customerId);
+
 }
